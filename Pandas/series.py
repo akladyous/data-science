bc14dee2e79f240492b23978f967d18a1e60f610
Boula Akladyous <akladyous@gmail.com>
2023-05-27 12:32:26 -0400

    Untracked files on Pandas

--- Pandas/series.py
+++ Pandas/series.py
@@ -3,4 +3,5 @@ import pandas as pd
 nums = ["one", "two", "three", "four", "five"]
 nums_series = pd.Series(nums, [chr(x + 65) for x in range(len(nums))])
 
-print(nums)
+country_list = "United States, France, Germany, Italy"
+countries = pd.Series(country_list.split(" "))
