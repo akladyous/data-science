15dbb4abb367c302f1abf27a10d56f104e656169
Boula Akladyous <akladyous@gmail.com>
2023-05-29 16:33:06 -0400

    Untracked files on data-science

--- NumPy/numpy_demo.py
+++ NumPy/numpy_demo.py
@@ -3,8 +3,9 @@ import numpy as np
 
 arr1 = np.array([x for x in range(10)])
 
-print("np.array  :", arr1)
-print("np.array  :", arr1.ndim)
-print("np.array  :", arr1.shape)
-print("np.array  :", arr1.size)
-print("np.array  :", arr1.dtype)
+print("np.array {0:>20} ".format(arr1))
+print("np.array {0:>20} ".format(arr1.ndim))
+print("np.array {0:>20} ".format(arr1.shape))
+print("np.array {0:>20} ".format(arr1.size))
+print("np.array {0:>20} ".format(arr1.dtype))
+# print("np.array {0:>20} ".format(str(arr1d)))
