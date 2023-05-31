60cfb630b023fd4201e2ba4272240b38cfcc6410
Boula Akladyous <akladyous@gmail.com>
2023-05-31 01:41:04 -0400

    Untracked files on data-science

--- Pandas/series.py
+++ Pandas/series.py
@@ -13,5 +13,22 @@ random_integers = rng.integers(1, 10, 10)
 random_decimals = rng.random(10)
 normal_distribution = rng.normal(2, 1, 10)
 
-s1 = Series(random_integers)
+s1 = Series(
+    random_integers,
+    index=[f"n_{x}" for x in random_integers],
+    dtype=np.int8,
+    name="Random Integers",
+)
 print(s1)
+
+s2_data = {chr(97 + x): x for x in range(5)}
+print(
+    "indexes = : ",
+    list(map(lambda x: x.upper(), s2_data.keys())),
+)
+s2 = pd.Series(
+    data=s2_data,
+    dtype=np.int8,
+    name="Dict Comprehension",
+)
+print(s2)
