97795649783c4f7c7555925fced4415ded9285d1
Boula Akladyous <akladyous@gmail.com>
2023-05-31 10:01:45 -0400

    Untracked files on data-science

--- Pandas/series.py
+++ Pandas/series.py
@@ -1,19 +1,15 @@
 import pandas as pd
-from pandas import Series
 import numpy as np
 
 nums = ["one", "two", "three", "four", "five"]
 nums_series = pd.Series(nums, [chr(x + 65) for x in range(len(nums))])
-
-country_list = "United States, France, Germany, Italy"
-countries = pd.Series(country_list.split(" "))
 
 rng = np.random.default_rng(123)
 random_integers = rng.integers(1, 10, 10)
 random_decimals = rng.random(10)
 normal_distribution = rng.normal(2, 1, 10)
 
-s1 = Series(
+s1 = pd.Series(
     random_integers,
     index=[f"n_{x}" for x in random_integers],
     dtype=np.int8,
@@ -29,7 +25,12 @@ s2 = pd.Series(
     dtype=np.int8,
     name="Dict Comprehension",
 )
-
 print(s2)
-result = s2.get(6, False)
-print(result)
+print("-" * 50)
+
+s3 = pd.read_json(
+    "./data/countries_with_code.json",
+)
+countries = s3.get("name")
+
+print()
