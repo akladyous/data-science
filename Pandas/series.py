850bfcd2704694309ef5f3e641d0ac2fa13d7426
Boula Akladyous <akladyous@gmail.com>
2023-05-31 01:16:52 -0400

    Untracked files on data-science

--- Pandas/series.py
+++ Pandas/series.py
@@ -1,7 +1,16 @@
 import pandas as pd
+from pandas import Series
+import numpy as np
 
 nums = ["one", "two", "three", "four", "five"]
 nums_series = pd.Series(nums, [chr(x + 65) for x in range(len(nums))])
 
 country_list = "United States, France, Germany, Italy"
 countries = pd.Series(country_list.split(" "))
+
+rng = np.random.default_rng(123)
+random_integers = rng.integers(1, 10, 10)
+random_decimals = rng.random(10)
+
+print("random_integers : ", random_integers)
+print("random_decimals : ", random_decimals)
