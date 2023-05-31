caa92491af34d60edd5cf34b9854e2415a0fe5b8
Boula Akladyous <akladyous@gmail.com>
2023-05-31 11:01:49 -0400

    Untracked files on data-science

---
+++ Pandas/dataframe_demo.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+countries_db = pd.read_csv("./data/countries_with_code.csv.gz", index_col="code")
+countries = countries_db["name"]
+print("countries : ", countries)
