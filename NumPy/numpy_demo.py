b9fea8c131f47578310671d5342794feec46a723
Boula Akladyous <akladyous@gmail.com>
2023-06-27 23:58:34 -0400

    Untracked files on data-science

--- NumPy/numpy_demo.py
+++ NumPy/numpy_demo.py
@@ -10,13 +10,22 @@ import numpy as np
 # print("np.array {0:>20} ".format(arr1.dtype))
 # print("np.array {0:>20} ".format(str(arr1d)))
 
-rng = np.random.default_rng(5)
-x = rng.integers(1, 10, 5)
-print(np.sum(x))
+rng = np.random.default_rng(31)
+rand_int = rng.integers(1, 10, 5)
+print(rand_int)
 
-r1 = rng.random(10)
-print(r1)
+r_float = rng.random(10)
+print(r_float)
+print("-" * 50)
+# Create a 2D array
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr)
+# Flip the array along the first axis (rows)
+flipped_arr_axis0 = np.flip(arr, axis=0)
+print(arr, " ", flipped_arr_axis0)
 
-import math
+# Flip the array along the second axis (columns)
+flipped_arr_axis1 = np.flip(arr, axis=1)
 
-math.sin()
+print("-" * 50)
+print(flipped_arr_axis1)
