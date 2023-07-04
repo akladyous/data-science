2104eb2124f9d786ca667a10086d4692182a6687
Boula Akladyous <akladyous@gmail.com>
2023-07-04 12:17:43 -0400

    Untracked files on data-science

--- pandas/dataframe_demo.py
+++ pandas/dataframe_demo.py
@@ -5,5 +5,4 @@ countries_db = pd.read_csv("./data/countries_with_code.csv.gz", index_col="code"
 countries = countries_db["name"]
 # print("countries : ", countries)
 
-# italia_egitto = countries_db.loc["IT":"EG"]
 print(countries_db.loc["EG":"IT"])
