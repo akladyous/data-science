9b828c89c44377cd2db8799f2f6d6c1fd48b9654
Boula Akladyous <akladyous@gmail.com>
2024-03-13 14:00:28 -0400

    Untracked files on data-science

---
+++ metaclasses.py
@@ -0,0 +1,18 @@
+class Point:
+    x: int
+    y: int
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"Point {self.x} {self.y}"
+
+    def __eq__(self, other: object) -> bool:
+        return self.x == other.x and self.y == other.y
+
+
+p1 = Point(1, 2)
+p2 = Point(1, 2)
+print(p1 == p2)
