d55ed06f34a3befcca9bc145d6a4df7583b28cc3
Boula Akladyous <akladyous@gmail.com>
2024-08-19 20:43:04 -0400

    Untracked files on data-science

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
-pip3 install --upgrade pip3
+pip install --upgrade pip
 
 pip install jupyterlab notebook ipywidgets qtconsole numpy pandas scikit-learn scipy statsmodels requests matplotlib seaborn
 
