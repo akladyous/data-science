5a371c914dbcb784833d35a45287f3ff86908ee0
Boula Akladyous <akladyous@gmail.com>
2024-08-21 10:38:02 -0400

    deleted dev dependency installing script

--- install.sh
+++
@@ -1,6 +0,0 @@
-
-pip install --upgrade pip
-
-pip install jupyterlab notebook ipywidgets qtconsole numpy pandas scikit-learn scipy statsmodels requests matplotlib seaborn
-
-pip3 install -r ./requirements.txt
